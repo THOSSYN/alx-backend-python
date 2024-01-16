4068537aba4acfc8c7e4a29e4416f58ed415245c
THOSSYN <alextosin5@gmail.com>
2024-01-16 13:26:05 +0000

    Updated 2-measure_runtime.py

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,7 +8,7 @@ import time
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
-async def measure_runtime():
+async def measure_runtime() -> float:
     """ A function that measure the runtime of
        an asynchronous process repeating four times
     """
