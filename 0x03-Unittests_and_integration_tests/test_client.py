7eba3345eb131b95b13f09d468dedf5f6362e4bd
THOSSYN <alextosin5@gmail.com>
2024-02-05 13:44:51 +0100

    Fix test error for has_license

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -62,13 +62,13 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(result, expected_repos)
 
     @parameterized.expand([
-        ({"license": {"key": "my_license"}}, "my_license", False),
+        ({"license": {"key": "my_license"}}, "my_license", True),
         ({"license": {"key": "other_license"}}, "my_license", False)
     ])
     def test_has_license(self, repo, license_key, expected):
         """Test has_license method"""
         git_org = GithubOrgClient('google')
-        result = git_org.has_license(repo, license)
+        result = git_org.has_license(repo, license_key)
 
         self.assertEqual(result, expected)
 
@@ -113,6 +113,7 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def test_public_repos_with_license(self):
         """Tests for public_repos with license"""
         pass
+
 
 if __name__ == '__main__':
     unittest.main()
